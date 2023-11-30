d2237e77f05cfe84408081b0cc2cb055ce329d80
Oz0neX <kingyasir8327@gmail.com>
2023-11-29 16:35:20 -0800

    W

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ root.render(
 	</React.StrictMode>
 );
 
+//https://www.figma.com/file/6QBurBerYup7nbo9yFnYSw/Untitled?type=design&node-id=0%3A1&mode=design&t=V53C7wprN2d42KnU-1
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
